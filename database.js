ce31120aa0cde6d094bff10ccf9ecdc23341024f
GnagetEmil <emil@gnaget.se>
2023-03-23 15:11:07 +0100

    Added JWT authentication for admin and login

---
+++ database.js
@@ -0,0 +1,79 @@
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('./usersDb.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Database created.')
+});
+
+function initDb() {
+    db.serialize(function () {
+        db.run("DROP TABLE IF EXISTS users;");
+        db.run("CREATE TABLE IF NOT EXISTS users(username TEXT NOT NULL PRIMARY KEY, name TEXT, role TEXT CHECK(role in('student1', 'student2', 'teacher','admin')), password TEXT NOT NULL);");
+    })
+}
+
+function getUsers() {
+    return new Promise((resolve, reject) => {
+        db.all("SELECT * FROM users", (err, res) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(res);
+            }
+        });
+    });
+}
+
+function getPasswordForUser(usernameToSearchFor) {
+    return new Promise((resolve, reject) => {
+        const sql = "SELECT password FROM users where username = ?";
+        db.get(sql, [usernameToSearchFor], (err, row) => {
+            if (err) {
+                return reject(err);
+            }
+            if (!row) {
+                return reject(new Error("User not found"));
+            }
+            resolve(row.password);
+        });
+    });
+}
+
+async function addUser(username, name, role, password) {
+    if (await usernameExists(username)) {
+        console.log("username already exists")
+    }
+    else {
+        return new Promise((resolve, reject) => {
+            db.run('INSERT INTO users (username, name, role, password) VALUES (?, ?, ?, ?)', [username, name, role, password], (err) => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
+    }
+}
+
+function usernameExists(usernameToSearchFor) {
+    let sql = "SELECT username FROM users"
+    return new Promise((myResolve, myReject) => {
+        db.all(sql, (err, rows) => {
+            if (err) {
+                console.log("ERROR")
+            }
+            else {
+                for (let i = 0; i < rows.length; i++) {
+                    if (rows[i].username === usernameToSearchFor) {
+                        myResolve(true)
+                    }
+                }
+                myResolve(false)
+            }
+        })
+    });
+}
+
+module.exports = { addUser, getUsers, getPasswordForUser }
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
+const db = require('./database')
+const bcrypt = require('bcrypt')
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
@@ -14,16 +16,37 @@ app.get('/', (req, res) => {
     res.redirect("/identify")
 })
 
-app.post('/identify', (req, res) => {
-    const username = req.body.password
-    const token = jwt.sign(username, process.env.ACCESS_TOKEN_SECRET)
-    currentKey = token
-    currentPassword = username
-    res.redirect("/granted")
+app.post('/identify', async (req, res) => {
+
+    if (req.body.userId && req.body.password) {
+        // Gets the encrypted password from the db
+        var correctPassword = await db.getPasswordForUser(req.body.userId)
+        // Compares the encrypted passwords
+        var passwordMatches = await bcrypt.compare(req.body.password, correctPassword)
+        if (passwordMatches) {
+            const username = req.body.password
+            const token = jwt.sign(username, process.env.ACCESS_TOKEN_SECRET)
+            currentKey = token
+            currentPassword = username
+            res.redirect("/granted")
+        }
+        else {
+            res.redirect("/identify")
+        }
+    } else {
+        res.redirect("/identify")
+    }
+
+
 })
 
 app.get('/identify', (req, res) => {
     res.render("identify.ejs")
+})
+
+app.get('/admin', authenticateToken, async (req, res) => {
+    users = await db.getUsers();
+    res.render("admin.ejs", users)
 })
 
 
@@ -42,7 +65,14 @@ app.get('/granted', authenticateToken, (req, res) => {
     res.render("start.ejs")
 })
 
-
+async function addUser(username, name, role, password) {
+    let encryptedPassword = await bcrypt.hash(password, 10);
+    await db.addUser(username, name, role, encryptedPassword)
+}
+addUser('id1', 'user1', 'student1', 'password')
+addUser('id2', 'user2', 'student2', 'password2')
+addUser('id3', 'user3', 'teacher', 'password3')
+addUser('admin', 'admin', 'admin', 'admin')
 
 app.listen(8000, () => {
     console.log("Server is running on port 8000")
